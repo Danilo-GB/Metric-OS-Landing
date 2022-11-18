c69b2419855a0165b1915a2fc0700d7bf67b5852
Danilo Garcia <82009562+Danilo-GB@users.noreply.github.com>
2022-11-17 23:07:28 -0500

    Update AppHeader.vue

--- src/layouts/AppHeader.vue
+++ src/layouts/AppHeader.vue
@@ -27,7 +27,8 @@
             <b-nav-item href="#" v-scroll-to="'#elemProduct'"
               >Project</b-nav-item
             >
-            <b-nav-item href="https://metric-os-docs.netlify.app/" target="_blank" rel="noopener noreferrer">Docs</b-nav-item>
+            <b-nav-item href="https://metric-os-docs.netlify.app/" target="_blank" rel="noopener noreferrer">Docs web</b-nav-item>
+            <b-nav-item href="https://bit.ly/metric-os-user-manual" target="_blank" rel="noopener noreferrer">Docs PDF</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-container>
